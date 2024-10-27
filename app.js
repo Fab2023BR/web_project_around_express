b21c574e14a81c648798022623969340bc5c6c87
Fabiana Storer <fabiana.storer@gmail.com>
2024-10-27 11:49:56 +0100

    import mongoose

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import userRouter from './routes/users.js';
 import cardRouter from './routes/cards.js';
+import mongoose from mongoose;
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/aroundb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
 
 app.use('/users', userRouter);
 app.use('/cards', cardRouter);
